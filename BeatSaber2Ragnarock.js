98a0892af16694115955f3d794279bd09c7ad48f
voupidia <>
2021-02-02 01:50:09 +0100

    Version 0.2.0 which accepts a info.dat file and converts all difficulty maps of a song

--- BeatSaber2Ragnarock.js
+++ BeatSaber2Ragnarock.js
@@ -1,51 +1,74 @@
+const version = "0.2.0";
+
 const fs = require("fs");
 const path = require("path");
 
-// const argv = process.argv.slice(2);
-// const filename = argv[0];
-const filename = process.argv[2];
-let nodesToDelete = [];
-const timeEpsilon = 0.11;
+const infoFileName = process.argv[2];
+const dirPath = path.dirname(fs.realpathSync(infoFileName));
 
-if (filename) {
-	let data = fs.readFileSync(filename);
+let nodesToDelete = [];
+const timeEpsilon = 0.16;
+
+if (infoFileName) {
+	let data = fs.readFileSync(infoFileName);
 
 	if (data) {
-		let song = JSON.parse(data);
-		let notes = song._notes;
-
-		// Eliminate vertical stacked notes before squashing all into one line layer to avoid double notes
-		notes.forEach((note, index) => {
-			let nextNotes = notes.slice(index + 1, index + 4);
-			if (nextNotes.filter(neighborNote => Math.abs(note._time - neighborNote._time) < timeEpsilon && neighborNote._lineIndex === note._lineIndex).length > 0) {
-				nodesToDelete.push(note);
+		let info = JSON.parse(data);
+		for (let difficultyBeatmapSet of info._difficultyBeatmapSets) {
+			for (let difficultyBeatmap of difficultyBeatmapSet._difficultyBeatmaps) {
+				const filePath = path.join(dirPath, difficultyBeatmap._beatmapFilename);
+				let songData = fs.readFileSync(filePath);
+				if (songData) {
+					let song = JSON.parse(songData);
+					let notes = song._notes;
+			
+					// Eliminate vertical stacked notes before squashing all into one line layer to avoid double notes
+					notes.forEach((note, index) => {
+						let nextNotes = notes.slice(index + 1, index + 4);
+						if (nextNotes.filter(neighborNote => Math.abs(note._time - neighborNote._time) < timeEpsilon && neighborNote._lineIndex === note._lineIndex).length > 0) {
+							nodesToDelete.push(note);
+						}
+					});
+					notes = notes.filter(note => !nodesToDelete.includes(note));
+					
+					// squash all notes into line layer 1, make them red, set cut direction to downward
+					notes.forEach(note => {
+						note._type = 0;
+						note._cutDirection = 1;
+						note._lineLayer = 1;
+					});
+					
+					// Ensure there are only so many notes per row as people have arms. 
+					nodesToDelete = [];
+					notes.forEach((note, index) => {
+						let nextNotes = notes.slice(index + 1, index + 4);
+						if (nextNotes.filter(neighborNote => Math.abs(note._time - neighborNote._time) < timeEpsilon).length > 1) {
+							nodesToDelete.push(note);
+						}
+					});
+					notes = notes.filter(note => !nodesToDelete.includes(note));
+			
+					song._notes = notes;
+					songData = JSON.stringify(song);
+					fs.writeFileSync(filePath, songData);
+					console.log(`${difficultyBeatmap._beatmapFilename} converted to Ragnaröck!`);
+				} else {
+					console.error("ERROR: Failed to load beatmap file!");
+				}
 			}
-		});
-		notes = notes.filter(note => !nodesToDelete.includes(note));
-		
-		// squash all notes into line layer 1, make them red, set cut direction to downward
-		notes.forEach(note => {
-			note._type = 0;
-			note._cutDirection = 1;
-			note._lineLayer = 1;
-		});
-		
-		// Ensure there are only so many notes per line as people have arms. 
-		nodesToDelete = [];
-		notes.forEach((note, index) => {
-			let nextNotes = notes.slice(index + 1, index + 4);
-			if (nextNotes.filter(neighborNote => Math.abs(note._time - neighborNote._time) < timeEpsilon).length > 1) {
-				nodesToDelete.push(note);
-			}
-		});
-		notes = notes.filter(note => !nodesToDelete.includes(note));
-
-		song._notes = notes;
-		data = JSON.stringify(song);
-		fs.writeFileSync(filename, data);
-		console.log(`${filename} converted to Ragnaröck!`);
+		}
+		if (!info._customData._editors) {
+			info._customData._editors = {};
+		}
+		info._customData._editors.BeatSaber2Ragnarock = { 
+			version: version,
+			source: "https://github.com/voupidia/BeatSaber2Ragnarock"
+		};
+		info._customData._editors._lastEditedBy = "BeatSaber2Ragnarock";
+		data = JSON.stringify(info, null, 4);
+		fs.writeFileSync(infoFileName, data);
 	} else {
-		console.error("ERROR: Failed to load file!");
+		console.error("ERROR: Failed to load info file!");
 	}
 } else {
 	console.error("ERROR: No file name given!");
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # BeatSaber2Ragnarock
-Song Converter from Beat Saber to Ragnaröck
+This is a converter script for custom songs from [Beat Saber](https://store.steampowered.com/app/620980/Beat_Saber/ "Beat Saber Steam Page") to [Ragnaröck](https://store.steampowered.com/app/1345820/Ragnarock/ "Ragnaröck Steam Page") written in JavaScript.
+When you want to play a song in Ragnaröck, nobody mapped yet but you can find it for Beat Saber ([i.e. BEAST SABER](https://bsaber.com/ "BEAST SABER website")), this script can help you to converting it to give you a base for improving upon.
+But be aware, unedited converted scripts can never be as good, as original ones.
+Some patterns just don't work that well in Ragnaröck.
+- - -
+
+
+## What it does
+* Checking for and removing stacked notes, so the later steps don't produce multiple runes on top of each other
+* Applying the important steps of the how-to-convert-map guide in the [RagnaSong Discord](https://discord.gg/vkbDDwhV "RagnaSong Discord Invite")
+* Sanity checking for more than 2 runes in a row and deleting the extra ones (for example horizontal swipes in Beat Saber)
+- - -
+
+
+## How to use it
+![How to use the script](images/howto.gif)
+
+* You'll need to have [Node.js](https://nodejs.org/ "Node.js website") installed to execute JavaScript files locally
+* Open a command line and [navigate](https://www.computerhope.com/issues/chusedos.htm "How to use the Windows command line") to the directory where you saved the [BeatSaber2Ragnarock.js](BeatSaber2Ragnarock.js) script
+* Execute the [BeatSaber2Ragnarock.js](BeatSaber2Ragnarock.js) script with node and the info.dat of the song you want to convert as only argument
+```
+> node BeatSaber2Ragnarock.js C:\Path\To\Your\info.dat
+```
+* Play and improve upon the map :metal:
